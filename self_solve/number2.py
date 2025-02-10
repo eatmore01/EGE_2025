50432f8c351bfc697c1c4547cb4d2c867b2485b6
eatmore01 <kirillzuev987@gmail.com>
2025-02-11 07:11:53 +0800

    update number 1 and 2

---
+++ self_solve/number2.py
@@ -0,0 +1,11 @@
+print("x y z w")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2): 
+                F = (((w <= y) <= x) or not z)
+                if F == 0:
+                    print(x, y, z, w)
+
+
+# zywx
