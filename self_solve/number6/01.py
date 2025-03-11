f9c51b744e976b1f19ca3f1bd716ad46c858a2fd
eatmore01 <kirillzuev987@gmail.com>
2025-03-11 11:08:06 +0800

    add some for number 6 and 7

--- self_solve/number6/01.py
+++ self_solve/number6/01.py
@@ -1,1 +1,27 @@
 from turtle import *
+tracer(0) # выключить анимацию
+left(90) # нужно повернуть на ось оординат ( y ) по дефолту направлена на ось абцисс ( x )
+k = 20 # задать мастшаб ( по дефолту один я так понял )
+screensize(3000, 3000)
+for _ in range(9):
+    forward(22 * k)
+    right(90)
+    forward(22 * k)
+    right(90)
+penup() # поднять хвост
+forward(1 * k)
+right(90)
+forward(5 * k)
+left(90)
+pendown()
+for _ in range(9):
+    forward(53 * k)
+    right(90)
+    forward(75 * k)
+    right(90)
+penup()
+for x in range(-60, 60):
+    for y in range(-60, 60):
+        goto(x * k, y * k)
+        dot(5)
+done() # что бы рисунок никуда не ушел
